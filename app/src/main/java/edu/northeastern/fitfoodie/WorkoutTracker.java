731db4f44ea3bc616bed5ca4a09f51f9da960d27
Anant Moudgalya <moudgalya.a@northeastern.edu>
2023-04-17 21:33:50 -0400

    - workout upload completed

--- app/src/main/java/edu/northeastern/fitfoodie/WorkoutTracker.java
+++ app/src/main/java/edu/northeastern/fitfoodie/WorkoutTracker.java
@@ -6,22 +6,58 @@ public class WorkoutTracker {
     String type;
     String startTime;
     String endTime;
-
-
-
     double distance;
     Date workoutDate;
     double averageSpeed;
+    double caloriesBurnt; // new attribute
 
-    WorkoutTracker(String type, String startTime, String endTime, double averageSpeed, double distance, Date workoutDate){
+    long workoutDuration;
+
+    WorkoutTracker(String type, String startTime, String endTime, double averageSpeed, double distance, Date workoutDate, long workoutDuration, double caloriesBurnt){
         this.type = type;
         this.startTime = startTime;
         this.endTime = endTime;
         this.distance = distance;
         this.workoutDate = workoutDate;
         this.averageSpeed = averageSpeed;
+        this.workoutDuration = workoutDuration;
+        this.caloriesBurnt = caloriesBurnt;
     }
 
+    // getters
+    public String getType() {
+        return type;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public Date getWorkoutDate() {
+        return workoutDate;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public long getWorkoutDuration() {
+        return workoutDuration;
+    }
+
+    public double getCaloriesBurnt() {
+        return caloriesBurnt;
+    }
+
+    // setters
     public void setEndTime(String endTime) {
         this.endTime = endTime;
     }
@@ -32,5 +68,13 @@ public class WorkoutTracker {
 
     public void setAverageSpeed(double averageSpeed) {
         this.averageSpeed = averageSpeed;
+    }
+
+    public void setCaloriesBurnt(double caloriesBurnt) {
+        this.caloriesBurnt = caloriesBurnt;
+    }
+
+    public void setWorkoutDuration(long workoutDuration) {
+        this.workoutDuration = workoutDuration;
     }
 }
